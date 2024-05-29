2cd6998a5e0e9209d8dc954383534b6e9df2b427
Gavin OMelia <gavin@omelia.org>
2024-05-28 20:16:27 -0700

    Complete moviegoer

---
+++ moviegoer/moviegoer.rb
@@ -0,0 +1,21 @@
+class NotMovieClubMemberError < RuntimeError
+end
+
+class Moviegoer
+  def initialize(age, member: false)
+    @age = age
+    @member = member
+  end
+
+  def ticket_price
+    @age < 60 ? 15 : 10
+  end
+
+  def watch_scary_movie?
+    @age >= 18
+  end
+
+  def claim_free_popcorn!
+    @member ? "🍿" : (raise NotMovieClubMemberError.new)
+  end
+end

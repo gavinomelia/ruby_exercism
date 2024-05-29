f50d18f4ed35bc42290270c8273a11a5b5ed0a23
Gavin OMelia <gavin@omelia.org>
2024-05-28 21:32:58 -0700

    Complete resistor color duo

---
+++ resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,17 @@
+class ResistorColorDuo
+   COLOR_CODES = {
+    "black"  => 0,
+    "brown"  => 1,
+    "red"    => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green"  => 5,
+    "blue"   => 6,
+    "violet" => 7,
+    "grey"   => 8,
+    "white"  => 9
+  }
+  def self.value(colors)
+    colors.first(2).map(&COLOR_CODES).join.to_i
+  end
+end
